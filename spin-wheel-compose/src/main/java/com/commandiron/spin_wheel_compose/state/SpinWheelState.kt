482443842ec9940b24b757786f5dd8cec8b4a4a1
Mohammad Rezaei <rezaei.Moh@entekhabelectronic.com>
2024-01-01 16:19:44 +0330

    Add infiniteSpin and stopOn functions

--- spin-wheel-compose/src/main/java/com/commandiron/spin_wheel_compose/state/SpinWheelState.kt
+++ spin-wheel-compose/src/main/java/com/commandiron/spin_wheel_compose/state/SpinWheelState.kt
@@ -4,10 +4,16 @@ import androidx.annotation.IntRange
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import kotlinx.coroutines.delay
-import java.util.*
+import java.util.Random
 
 data class SpinWheelState(
     internal val pieCount: Int,
@@ -21,27 +27,30 @@ data class SpinWheelState(
 ) {
     internal var rotation by mutableStateOf(Animatable(startDegree))
     private var spinAnimationState by mutableStateOf(SpinAnimationState.STOPPED)
+    private val total = (360f * rotationPerSecond * (durationMillis / 1000))
 
     suspend fun animate(onFinish: (pieIndex: Int) -> Unit = {}) {
-        when(spinAnimationState) {
+        when (spinAnimationState) {
             SpinAnimationState.STOPPED -> {
-                spin(onFinish = onFinish)
+                randomSpin(onFinish)
             }
+
             SpinAnimationState.SPINNING -> {
                 reset()
             }
         }
     }
 
-    suspend fun spin(onFinish: (pieIndex: Int) -> Unit = {}) {
-        if(spinAnimationState == SpinAnimationState.STOPPED) {
+    suspend fun randomSpin(onFinish: (pieIndex: Int) -> Unit = {}) {
+        if (spinAnimationState == SpinAnimationState.STOPPED) {
 
             spinAnimationState = SpinAnimationState.SPINNING
 
             val randomRotationDegree = generateRandomRotationDegree()
 
             rotation.animateTo(
-                targetValue = (360f * rotationPerSecond * (durationMillis / 1000)) + (resultDegree ?: randomRotationDegree),
+                targetValue = (360f * rotationPerSecond * (durationMillis / 1000)) + (resultDegree
+                    ?: randomRotationDegree),
                 animationSpec = tween(
                     durationMillis = durationMillis,
                     delayMillis = delayMillis,
@@ -55,19 +64,64 @@ data class SpinWheelState(
 
             onFinish(resultIndex)
 
-            rotation.snapTo(resultDegree ?:randomRotationDegree)
+            rotation.snapTo(resultDegree ?: randomRotationDegree)
 
             spinAnimationState = SpinAnimationState.STOPPED
 
             autoSpinDelay?.let {
                 delay(it)
-                spin()
+                randomSpin()
             }
         }
     }
 
+    suspend fun infiniteSpin() {
+        if (spinAnimationState == SpinAnimationState.STOPPED) {
+
+            spinAnimationState = SpinAnimationState.SPINNING
+
+            rotation.animateTo(
+                targetValue = total,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = durationMillis,
+                        delayMillis = delayMillis,
+                        easing = LinearEasing
+                    )
+                )
+            )
+
+            autoSpinDelay?.let {
+                delay(it)
+                infiniteSpin()
+            }
+        }
+    }
+
+    suspend fun stopOn(itemIndex: Int) {
+        if (spinAnimationState == SpinAnimationState.SPINNING) {
+
+            val pieDegree = 360f / pieCount
+            val itemDegree = startDegree - (pieDegree * itemIndex).plus(pieDegree.div(2))
+
+            rotation.animateTo(
+                initialVelocity = rotation.velocity,
+                targetValue = total + itemDegree,
+                animationSpec = tween(
+                    durationMillis = durationMillis,
+                    delayMillis = 0,
+                    easing = easing
+                )
+            )
+
+            rotation.snapTo(itemDegree)
+
+            spinAnimationState = SpinAnimationState.STOPPED
+        }
+    }
+
     suspend fun reset() {
-        if(spinAnimationState == SpinAnimationState.SPINNING) {
+        if (spinAnimationState == SpinAnimationState.SPINNING) {
 
             rotation.snapTo(startDegree)
 
